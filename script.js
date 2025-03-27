4828533e559145b45015a71c16731a1253118d0a
Kshitiz Rana <93457779+KshitizRana@users.noreply.github.com>
2025-03-28 00:59:22 +0530

    Add pagination functionality

--- script.js
+++ script.js
@@ -1,40 +1,85 @@
-async function dataFetch() {
+// Features
+// Implement a search bar to filter books by title or author
+// Implement a sort feature to list books in Alphabetical order based on their title, date of release (publishedDate)
+let page = 1;
+async function dataFetch(page) {
   try {
-    const response = await fetch("https://api.freeapi.app/api/v1/public/books");
+    const response = await fetch(
+      `https://api.freeapi.app/api/v1/public/books?page=${page}&limit=10`
+    );
     const library = await response.json();
-    const books = library.data.data;
-    // console.log(books);
-    // console.log(library);
-    // console.log(typeof books);
-    return books;
+    return library;
   } catch (error) {
     console.log(error);
   }
 }
 
+const lib = await dataFetch(page);
 let contanier = document.querySelector(".container");
-const booksData = dataFetch();
-booksData.then((res) => {
-  res.forEach((book) => {
-    console.log(book);
-    // const bookCompleteData = {}
+function renderCards(books) {
+  contanier.innerHTML = "";
+  books.forEach((book) => {
     let card = document.createElement("div");
     card.classList = "bookcard";
     const bookVolume = book.volumeInfo;
-    const bookTitle = bookVolume.title;
-    const thumbnail = bookVolume.imageLinks.smallThumbnail;
-    const author = bookVolume.authors;
-    const publisher = bookVolume
-    const publishedDate = bookVolume.publishedDate;
     card.innerHTML = `
-              <img src="${thumbnail}" alt="" height="180" width="128" class="thumbnail" id="cardThumbnail" />
-
-        <title id="bookTitle">${bookTitle}</title>
-        <p id="bookAuthor">${author}</>
-        <p id="bookPublisher">${publisher}</p>
-        <p id="publishedDate">${publishedDate}</p>
-    `
-
+               <a href="${bookVolume.infoLink}" target="blank"> <img src="${bookVolume.imageLinks.smallThumbnail}" alt="" height="180" width="128"  class="thumbnail" id="cardThumbnail" />
+          
+          <p  class="cardText" id="bookTitle">Title: ${bookVolume.title}</p> 
+          <p class="cardText" id="bookAuthor">Author: ${bookVolume.authors}</> 
+          <p class="cardText" id="bookPublisher">Publisher: ${bookVolume.publisher}</p>
+          <p class="cardText" id="publishedDate">Published Date: ${bookVolume.publishedDate}</p> </a>
+      `;
     contanier.appendChild(card);
   });
+}
+renderCards(lib.data.data);
+
+// toggle functionality
+let grid = false;
+const gridBtn = document.getElementById("grid");
+const bookCard = document.getElementsByClassName("bookcard");
+gridBtn.addEventListener("click", () => {
+  if (grid) {
+    contanier.style.display = "flex";
+    // bookCard.style.width = `15%`;
+    grid = false;
+  } else {
+    contanier.style.display = "block";
+    // bookCard.style.width = `100%`;
+    grid = true;
+  }
+});
+
+//search
+const searchbar = document.getElementById("searchbar");
+function search() {
+  const value = this.value.toLowerCase().trim();
+  let authors = document.querySelectorAll("#bookAuthor");
+  let title = document.querySelectorAll("#bookTitle");
+  console.log(title);
+  console.log(authors);
+  if (value in authors || value in title) {
+  }
+}
+// console.log(authors);
+searchbar.addEventListener("input", search);
+
+//pagination
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const pageNumber = document.getElementById("pageNumber");
+prevBtn.addEventListener("click", async () => {
+  if (page > 1) {
+    page--;
+    pageNumber.innerText = page;
+    const lib = await dataFetch(page);
+    renderCards(lib.data.data);
+  }
+});
+nextBtn.addEventListener("click", async () => {
+  page++;
+  pageNumber.innerText = page;
+  const lib = await dataFetch(page);
+  renderCards(lib.data.data);
 });
